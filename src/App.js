3a85d811c1fea468df578d0eee73d7b27a3246e0
go367it <satyamkumar3001@gmail.com>
2021-10-23 14:32:05 +0530

    implemented redux in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import {incNumber, decNumber} from './actions/index'
 
 function App() {
+
+  const myState = useSelector((state) => state.changeNumber)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="d-flex justify-content-center">
+
+        <div className="card">
+          <div className="card-body">
+            
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button
+              onClick={()=>dispatch(decNumber())}
+              type="button" className="btn btn-primary">-</button>
+              <button type="button" disabled className="btn btn-outline-secondary">{myState}</button>
+              <button
+              onClick={()=>dispatch(incNumber())}
+              type="button" className="btn btn-primary">+</button>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
